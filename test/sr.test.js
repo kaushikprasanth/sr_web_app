b1d9b8ba2f3349c64be1731383b9a21cc44d19a6
kaushikprasanth <kaushikprasanth94@yahoo.com>
2021-04-01 15:42:51 -0500

    postman collection added, unit test cases updated

--- test/sr.test.js
+++ test/sr.test.js
@@ -8,29 +8,29 @@ chai.use(chaiHttp)
 
 let sr = {
     "buildingCode": "COH",
-    "description" : "Please turn up the AC in suite 1200D. It is too hot here.",
-    "createdBy": "Kaushik"
+    "description" : "Please turn up the AC in suite 1200D. It is too hot here."
    }
 
 describe('Service Request API',()=>{
 
-    // GET Route
-    // describe('GET /api/servicerequest',()=>{
-    //     it('should return all service requests',(done)=>{
-    //         chai.request(app)
-    //             .get("/api/servicerequest")
-    //             .end((err,res) =>{
-    //                 res.should.have.status(200)
-    //             done()
-    //         })
-    //     })
-    // })
+    // GET Route - To check auth
+    describe('GET /api/servicerequest',()=>{
+        it('should return 403 Forbidden',(done)=>{
+            chai.request(app)
+                .get("/api/servicerequest")
+                .end((err,res) =>{
+                    res.should.have.status(403)
+                done()
+            })
+        })
+    })
     //POST - Create a SR
     describe('POST /api/servicerequest',()=>{
         it('create a service requests',(done)=>{
            
             chai.request(app)
                 .post("/api/servicerequest")
+                .set('Cookie', 'name=Kaushik')
                 .send(sr)
                 .end((err,res) =>{
                     res.should.have.status(201)
@@ -44,14 +44,41 @@ describe('Service Request API',()=>{
      //GET - Get the created SR
      describe('GET /api/servicerequest/:id',()=>{
         it('create a service requests',(done)=>{
-            console.log(sr)
             chai.request(app)
                 .get("/api/servicerequest/"+sr['id'])
-
+                .set('Cookie', 'name=Kaushik')
                 .end((err,res) =>{
                     res.should.have.status(200)
                     res.body.should.be.a('object')
                     res.body.should.have.property('id')
+                done()
+            })
+        })
+    })
+    //PUT - Update the created SR
+    describe('PUT /api/servicerequest/:id',()=>{
+        it('update the service requests',(done)=>{
+            let upd_sr = {
+                currentStatus:"InProgress"
+            }
+            chai.request(app)
+                .put("/api/servicerequest/"+sr['id'])
+                .send(upd_sr)
+                .set('Cookie', 'name=Kaushik')
+                .end((err,res) =>{
+                    res.should.have.status(200)
+                done()
+            })
+        })
+    })
+     //Delete - delete the created SR
+     describe('DELETE /api/servicerequest/:id',()=>{
+        it('delete the service requests',(done)=>{
+            chai.request(app)
+                .delete("/api/servicerequest/"+sr['id'])
+                .set('Cookie', 'name=Kaushik')
+                .end((err,res) =>{
+                    res.should.have.status(201)
                 done()
             })
         })
